84a9b19fe1dfae797601cc33c2a0e579e4c73935
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-10-12 00:09:55 +0200

    Add 1 more procedure, rename 1 view.

--- oracle/procedures/type_definitions.sql
+++ oracle/procedures/type_definitions.sql
@@ -9,3 +9,16 @@ CREATE OR REPLACE TYPE osoby_wycieczki_type AS OBJECT
 );
 
 CREATE OR REPLACE TYPE osoby_wycieczki_table IS TABLE OF osoby_wycieczki_type;
+
+
+CREATE OR REPLACE TYPE wycieczki_type AS OBJECT
+(
+    "id wycieczki"    number,
+    "nazwa wycieczki" varchar2(100),
+    kraj              varchar2(50),
+    opis              varchar2(200),
+    "liczba miejsc"   number,
+    "data"            date
+);
+
+CREATE OR REPLACE TYPE wycieczki_table IS TABLE OF wycieczki_type;
