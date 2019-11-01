150951be30d66227f1de4e1dd166bba25b4b5611
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-11-01 16:11:43 +0100

    Add files for EF task.

---
+++ entity_framework/entity_framework_linq2entries/CategoryForm.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace entity_framework_linq2entries
+{
+    public partial class CategoryForm : Form
+    {
+        public CategoryForm()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
---
+++ entity_framework/entity_framework_linq2entries/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace entity_framework_linq2entries.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<entity_framework_linq2entries.ProdContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "entity_framework_linq2entries.ProdContext";
+        }
+
+        protected override void Seed(entity_framework_linq2entries.ProdContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
