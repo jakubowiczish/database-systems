aa64f428e1b2683678f632099567cdee1509fbad
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-11-05 23:31:21 +0100

    Add separate directory for necessary files for documentation.

---
+++ entity_framework/resources_for_documentation/ProdContext.cs
@@ -0,0 +1,18 @@
+﻿using entity_framework_linq2entries;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace entity_framework_linq2entries
+{
+    class ProdContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+    }
+}
