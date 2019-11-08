703174086b6f96a74a2789e7744200b6a7116b14
Jakub Płotnikowski <jakub77plotnikowski@gmail.com>
2019-11-08 21:47:48 +0100

    Update Program.cs

--- entity_framework/entity_framework_linq2entries/Program.cs
+++ entity_framework/entity_framework_linq2entries/Program.cs
@@ -19,7 +19,7 @@ namespace entity_framework_linq2entries
             ShowCategoryForm();
         }
 
-        static void ShowCategoryForm() 
+        static void ShowCategoryForm()
         {
             CategoryForm Form = new CategoryForm();
             Form.ShowDialog();
@@ -65,6 +65,26 @@ namespace entity_framework_linq2entries
             foreach (Product p in ctx.Products)
             {
                 Console.WriteLine(p.Name);
+            }
+        }
+
+        static void ShowCategoriesAndProducts(ProdContext ctx)
+        {
+            Console.WriteLine("List of products:");
+            foreach (Product p in ctx.Products)
+            {
+                ctx.Entry(p)
+                    .Reference(e => e.Category)
+                    .Load();
+
+                if (null != p.Category)
+                {
+                    Console.WriteLine("Product: {0} {1} in category: {2}", p.productId, p.Name, p.Category.Name);
+                }
+                else
+                {
+                    Console.WriteLine("Product: {0} {1} without category", p.productId, p.Name, p.Category.Name);
+                }
             }
         }
     }
