d34d302280775e995933bf192fb66f2d1b8fac66
removedporn <86824510+removedporn@users.noreply.github.com>
2022-06-06 00:40:21 +0800

    Delete entity_framework directory

--- entity_framework/documentation/resources/SetCategoryIdReadOnly.cs
+++
@@ -1,4 +0,0 @@
-this.dataGridViewTextBoxColumn1.DataPropertyName = "CategoryId";
-this.dataGridViewTextBoxColumn1.HeaderText = "CategoryId";
-this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
-this.dataGridViewTextBoxColumn1.ReadOnly = true;
